415f684d0a679583edf9e3ac5a03f07d0d6b675c
LuisHBeck <beck.henrique.luis@gmail.com>
2023-10-13 14:50:49 -0300

    feat: screens navigation

--- src/screens/Register/index.jsx
+++ src/screens/Register/index.jsx
@@ -1,12 +1,91 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Input, Button, Text } from "@rneui/themed";
+import { View, StyleSheet, Image, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const Register = () => {
-  return (
-    <View>
-      <Text>Tela Login</Text>
-    </View>
-  )
-}
+	const navigation = useNavigation();
+
+	const handleNavLogin = () => {
+		navigation.navigate("SigIn");
+	};
+
+	return (
+		<ScrollView style={styles.scrollView}>
+		  <View style={styles.container}>
+  			<Image
+  				style={styles.logo}
+  				source={require("../../../assets/becks-logo.png")}
+  			/>
+  			<Input
+  				containerStyle={{ width: "85%", marginTop: 90 }}
+  				style={{ color: "white" }}
+  				placeholder="Name"
+  			/>
+  			<Input
+  				containerStyle={{ width: "85%" }}
+  				style={{ color: "white" }}
+  				placeholder="PASSWORD"
+  				secureTextEntry={true}
+  			/>
+  			<Input
+  				containerStyle={{ width: "85%" }}
+  				style={{ color: "white" }}
+  				placeholder="CONFIRM PASSWORD"
+  				secureTextEntry={true}
+  			/>
+  			<Input
+  				containerStyle={{ width: "85%" }}
+  				style={{ color: "white" }}
+  				placeholder="CONFIRM PASSWORD"
+  				secureTextEntry={true}
+  			/>
+  			<Input
+  				containerStyle={{ width: "85%" }}
+  				style={{ color: "white" }}
+  				placeholder="CONFIRM PASSWORD"
+  				secureTextEntry={true}
+  			/>
+  			<Input
+  				containerStyle={{ width: "85%" }}
+  				style={{ color: "white" }}
+  				placeholder="CONFIRM PASSWORD"
+  				secureTextEntry={true}
+  			/>
+  			<Button
+  				containerStyle={{ width: 180, marginTop: 70 }}
+  				buttonStyle={{ borderColor: "FFFFFF" }}
+  				titleStyle={{ color: "white" }}
+  				title="REGISTER"
+  				type="Outline"
+  			/>
+  			<Text
+  				onPress={() => handleNavLogin()}
+  				h4
+  				h4Style={{ color: "white", marginTop: 80 }}
+  			>
+  				Login
+  			</Text>
+  		</View>
+		</ScrollView>
+	);
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1
+  },
+	container: {
+		flex: 1,
+		backgroundColor: "#121212",
+		paddingTop: 120,
+    paddingBottom: 60,
+		alignItems: "center",
+	},
+	logo: {
+		width: 120,
+		height: 120,
+	},
+});
 
 export default Register;
--- src/screens/SigIn/index.jsx
+++ src/screens/SigIn/index.jsx
@@ -1,12 +1,62 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { Input, Button, Text } from "@rneui/themed";
+import { View, StyleSheet, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const SigIn = () => {
-  return (
-    <View>
-      <Text>Tela Login</Text>
-    </View>
-  )
-}
+	const navigation = useNavigation();
+
+	const handleNavRegister = () => {
+		navigation.navigate("Register");
+	};
+
+	return (
+		<View style={styles.container}>
+			<Image
+				style={styles.logo}
+				source={require("../../../assets/becks-logo.png")}
+			/>
+			<Input
+				containerStyle={{ width: "85%", marginTop: 90 }}
+				style={{ color: "white" }}
+				placeholder="CPF / CNPJ"
+        keyboardType="number-pad"
+			/>
+			<Input
+				containerStyle={{ width: "85%" }}
+				style={{ color: "white" }}
+				placeholder="PASSWORD"
+				secureTextEntry={true}
+			/>
+			<Button
+				containerStyle={{ width: 180, marginTop: 70 }}
+				buttonStyle={{ borderColor: "FFFFFF" }}
+				titleStyle={{ color: "white" }}
+				title="LOGIN"
+				type="Outline"
+			/>
+			<Text
+				onPress={() => handleNavRegister()}
+				h4
+				h4Style={{ color: "white", marginTop: 80 }}
+			>
+				Resgiter
+			</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#121212",
+		paddingTop: 120,
+		alignItems: "center",
+	},
+	logo: {
+		width: 120,
+		height: 120,
+	},
+});
 
 export default SigIn;
